0d945d65d317abdbe374627975d967c3799c9dd5
UmmuRufauda <mariamabdallah711@gmail.com>
2022-02-14 16:33:38 +0300

    Register and validation

---
+++ Login.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=!, initial-scale=1.0">
+    <title>Register</title>
+    <link rel="stylesheet" href="css/dashstyle.css">
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/jquery.min.js"></script>
+</head>
+
+<body class="bg-primary">
+    <div class="container p-4">
+        <div class="card">
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-lg-5">
+                        <img src="images/login.png" alt="Loading" height="250" width="250">
+
+                    </div>
+                    <div class="col-lg-7">
+                        <div class="text-center">
+                            <h4 class="grey">Welcome Back</h4>
+
+                        </div>
+                        <form action="" method="">
+                            <div class="row mb-3">
+                                <input class="form-control rounded-pill" type="email" name="email"
+                                    placeholder="Email Address">
+                            </div>
+                            <div class="row mb-3">
+                                <input class="form-control rounded-pill" type="password" name="password"
+                                    placeholder="Password">
+                            </div>
+                            <div class="row mb-3">
+                                <a href="" class="rounded-pill btn btn-primary">Login
+                                </a>
+                            </div>
+                            <hr>
+                        </form>
+                        <div class="text-center">
+                            <a class="small" href="">Forgot Password</a>
+                        </div>
+                        <div class=" text-center">
+                            <a class="small" href="Register.php">Create An Account</a>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
---
+++ handle_registration.php
@@ -0,0 +1,47 @@
+<?php
+
+include"config.php";
+
+if(isset($_POST["register"])){
+    $firstname =  $_POST["firstname"];
+    $secondname =  $_POST["secondname"];
+    $email =  $_POST["email"];
+    $password =  $_POST["password"];
+    $cpassword =  $_POST["cpassword"];
+
+    //validate
+
+    if (strlen($password)<6){
+        $passError = "Password must have more than 6 characters";
+        echo $passError;
+
+    }else{
+        $storePass = password_hash($password, PASSWORD_DEFAULT);
+    }
+    if($cpassword!=$password){
+        $conPassErr = "Password do not match";
+        echo $conPassErr;
+    }
+    if(empty($passError) and empty($conPassErr)){
+     
+        $sql = "INSERT INTO `user`(`firstname`, `secondname`, `email`, `password`)
+        VALUES ('$firstname','$secondname','$email',' $storePass')";
+
+$result = mysqli_query($link, $sql);
+
+if($result){
+    echo"You have been registered";
+    header("lacation:Login.php");
+}else{
+    echo"Error executing query".mysqli_error($link);
+}
+
+ 
+}
+mysqli_close($link);
+}
+
+
+
+
+?>
